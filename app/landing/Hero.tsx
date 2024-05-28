28cfc2d2ec7e3180d07a4f792d80cc84a2ff510c
sujeetbh <110466409+sujeetbh@users.noreply.github.com>
2024-05-28 15:13:46 +0530

    Update Hero.tsx

--- app/landing/Hero.tsx
+++ app/landing/Hero.tsx
@@ -26,8 +26,8 @@ export function Hero() {
       <h1 className="relative max-w-[678px] text-6xl md:text-8xl text-neutral-300 text-center font-sans font-bold bg-clip-text text-transparent bg-gradient-to-r from-neutral-100 via-neutral-200 to-neutral-500">
         {heroValue && heroValue[0] ? heroValue[0].title : heroData.title}
       </h1>
-      <p className="text-lg md:text-2xl mt-4 text-neutral-400">{heroValue && heroValue[0] ? heroValue[0].subtitle : heroData.subtitle}</p>
-      <p className="text-lg md:text-2xl mt-2 text-neutral-400">{heroValue && heroValue[0] ? heroValue[0].description : heroData.description}</p>
+      <p className="text-lg md:text-2xl mt-4 text-neutral-300">{heroValue && heroValue[0] ? heroValue[0].subtitle : heroData.subtitle}</p>
+      <p className="text-lg md:text-2xl mt-2 text-neutral-300">{heroValue && heroValue[0] ? heroValue[0].description : heroData.description}</p>
       <div className="flex gap-6 mt-20 flex-col md:flex-row items-center">
         <div className="bg-white w-max text-black px-4 md:px-8 py-4 rounded-lg text-xl font-semibold cursor-pointer">
           <Link href="/journey" className="flex items-center gap-2">
