65af92c5831bb737678ceb5e8fd1878fc46a904f
sujeetbh <110466409+sujeetbh@users.noreply.github.com>
2026-02-05 18:38:14 +0530

    layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Qilla"| Blockchain & Cybersecurity Platform",
+  description: "Security beyond blockchain for tokenised assets",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+              <Navbar />
+              {children}
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
