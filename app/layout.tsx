35c03daf7175e0656f05bc4b1d1e0c7d5d9939b6
sujeetbh <110466409+sujeetbh@users.noreply.github.com>
2026-02-05 19:01:08 +0530

    Final layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Qilla"| Blockchain & Cybersecurity Platform",
+  title: "Qilla | Blockchain & Cybersecurity Platform",
   description: "Security beyond blockchain for tokenised assets",
 };
 
