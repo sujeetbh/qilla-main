3f548591164ca2c67e503f7b3bd4fe1d4592ed5c
sujeetbh <110466409+sujeetbh@users.noreply.github.com>
2026-02-23 16:43:59 +0530

    Update page.tsx

--- app/(protectedRoutes)/dashboard/hero/page.tsx
+++ app/(protectedRoutes)/dashboard/hero/page.tsx
@@ -1,7 +1,7 @@
 import { HeroLayout } from '@/components/dashboardUI/hero-layout'
 import React from 'react'
 import { fetchHeroData } from '../../_actions/heroSection/getHeroData'
-import { auth } from '@/app/auth'
+import { auth } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
 const Hero = async () => {
